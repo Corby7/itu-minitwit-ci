c012aeace75caa1a7af07bd8d4e8fd5d33e8cb93
Corbijn Bulsink <jbul@itu.dk>
2026-02-22 14:22:14 +0100

    undo

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -2,6 +2,6 @@ source ~/.bash_profile
 
 cd /minitwit
 
-/usr/bin/docker compose -f docker-compose.yml pull
-/usr/bin/docker compose -f docker-compose.yml up -d
-/usr/bin/docker pull $DOCKER_USERNAME/flagtoolimage:latest
+docker compose -f docker-compose.yml pull
+docker compose -f docker-compose.yml up -d
+docker pull $DOCKER_USERNAME/flagtoolimage:latest
