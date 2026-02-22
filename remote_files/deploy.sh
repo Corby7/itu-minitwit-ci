513c7edeb431bc4c05f840928f00d08e09a31d0e
Corbijn Bulsink <jbul@itu.dk>
2026-02-22 14:10:32 +0100

    fix?

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -2,6 +2,6 @@ source ~/.bash_profile
 
 cd /minitwit
 
-docker compose -f docker-compose.yml pull
-docker compose -f docker-compose.yml up -d
-docker pull $DOCKER_USERNAME/flagtoolimage:latest
+/usr/bin/docker compose -f docker-compose.yml pull
+/usr/bin/docker compose -f docker-compose.yml up -d
+/usr/bin/docker pull $DOCKER_USERNAME/flagtoolimage:latest
